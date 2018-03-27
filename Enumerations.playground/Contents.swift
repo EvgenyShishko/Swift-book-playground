65109f0e042fe6e241e0505827de61fe921d1e95
Evgeny Shishko <Shishko.E.Ev@sberbank.ru>
2018-03-27 19:40:18 +0300

    Added playground about enums

---
+++ Enumerations.playground/Contents.swift
@@ -0,0 +1,71 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+// ASSOSIATED VALUES
+// Assosiated values are stored inside cases of enums
+enum Barcode {
+    case upc(Int?, Int?, Int, Int)
+    case qrCode(String)
+}
+
+var barcode = Barcode.upc(nil, nil, 2, 3)
+switch barcode {
+case .upc(let a, let b, let c, let d):
+    print("agagagagag")
+    print(a) // nil
+case .qrCode(let line):
+    print("line: \(line)")
+}
+
+// RAW VALUES
+// Raw values stored in each case and can be used by .rawValue
+enum RestApiMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case delete = "DELETE"
+}
+
+// Implicitly assigned raw values
+enum Numbers: Int {
+    case zero = 0, one, two, three, four
+}
+print(Numbers.two.rawValue + 3)
+
+// Init enum with raw value (always optional)
+let possibleNumber = Numbers.init(rawValue: 8)
+
+// RECURSIVE ENUMS
+
+// `indirect` keyword indicates that case has another Enum as assosiated value
+enum ArithmeticExpression {
+    case number(Int)
+    indirect case addition(ArithmeticExpression, ArithmeticExpression)
+    indirect case multiplication(ArithmeticExpression, ArithmeticExpression)
+}
+
+// You can also add `indirect` before `enum` keyword to make all case indirect
+indirect enum ArithmeticExpressionIndirect {
+    case number(Int)
+    case addition(ArithmeticExpression, ArithmeticExpression)
+    case multiplication(ArithmeticExpression, ArithmeticExpression)
+}
+
+func evaluate(_ expression: ArithmeticExpression) -> Int {
+    switch expression {
+    case let .number(value):
+        return value
+    case let .addition(left, right):
+        return evaluate(left) + evaluate(right)
+    case let .multiplication(left, right):
+        return evaluate(left) * evaluate(right)
+    }
+}
+
+let five = ArithmeticExpression.number(5)
+let four = ArithmeticExpression.number(4)
+let sum = ArithmeticExpression.addition(five, four)
+let product = ArithmeticExpression.multiplication(sum, ArithmeticExpression.number(2))
+
+print(evaluate(product))
